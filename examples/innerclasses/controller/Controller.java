5fda846dcb955b633ad1fcdfe56a892d0af55c40
grimble <grimble@gmx.com>
2018-10-12 23:45:31 +0700

    Holding Your Objects Ex. 14

--- examples/innerclasses/controller/Controller.java
+++ examples/innerclasses/controller/Controller.java
@@ -3,19 +3,38 @@
 package innerclasses.controller;
 import java.util.*;
 
+//public class Controller {
+//  // A class from java.util to hold Event objects:
+//  private List<Event> eventList = new ArrayList<Event>();
+//  public void addEvent(Event c) { eventList.add(c); }
+//  public void run() {
+//    while(eventList.size() > 0)
+//      // Make a copy so you're not modifying the list
+//      // while you're selecting the elements in it:
+//      for(Event e : new ArrayList<Event>(eventList))
+//        if(e.ready()) {
+//          System.out.println(e);
+//          e.action();
+//          eventList.remove(e);
+//        }
+//  }
+//} ///:~
+
 public class Controller {
   // A class from java.util to hold Event objects:
-  private List<Event> eventList = new ArrayList<Event>();
+  private List<Event> eventList = new LinkedList<Event>();
   public void addEvent(Event c) { eventList.add(c); }
   public void run() {
     while(eventList.size() > 0)
       // Make a copy so you're not modifying the list
       // while you're selecting the elements in it:
-      for(Event e : new ArrayList<Event>(eventList))
+      for(Iterator<Event> it= (new LinkedList<Event>(eventList)).iterator(); it.hasNext(); ) {
+        Event e= it.next();
         if(e.ready()) {
           System.out.println(e);
           e.action();
-          eventList.remove(e);
+          it.remove();
         }
+      }
   }
 } ///:~
